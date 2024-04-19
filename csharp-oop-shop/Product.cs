0c1e8d6351fa0a2a3179012fd6b6e3e944a002d2
Samuele-Benato <samuele.benato@gmail.com>
2024-04-19 15:59:18 +0200

    feat : add test product

--- csharp-oop-shop/Product.cs
+++ csharp-oop-shop/Product.cs
@@ -8,23 +8,7 @@ namespace csharp_oop_shop
 {
     internal class Product
     {
-        /*
-        Nel progetto csharp-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop.
-        Un prodotto è caratterizzato da:
-        - codice (numero intero)
-        - nome
-        - descrizione
-        - prezzo
-        - iva
-        Usate opportunamente i livelli di accesso (public, private), i costruttori,
-        i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
-        alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
-        Il codice prodotto sia accessibile solo in lettura
-        Gli altri attributi siano accessibili sia in lettura che in scrittura
-        Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
-        Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
-        Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interargirci per testare tutti i metodi che avete previsto
-        */
+       
         private int _code;
         public int Code
         {
@@ -50,6 +34,20 @@ namespace csharp_oop_shop
             return rnd.Next(10000, 99999);
         }
 
+        public decimal GetPrice()
+        {
+            return Price;
+        }
 
+        public decimal PriceWhitIva()
+        {
+            decimal priceWhitIva = Price * (1 + IVA / 100);
+            return Math.Round(priceWhitIva, 2); 
+        }
+
+        public string ExtendedName()
+        {
+            return Code + " " + Name;
+        }
     }
 }
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,10 +1,18 @@
 ﻿namespace csharp_oop_shop
 {
-    internal class Program
+    internal class Program 
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Product evianXBoolean = new Product("Acqua naturale Evian x Boolean", "Sofisticata acqua naturale", 10.50m, 22);
+
+            Console.WriteLine("Product Information:");
+            Console.WriteLine("Code: " + evianXBoolean.Code);
+            Console.WriteLine("Name: " + evianXBoolean.Name);
+            Console.WriteLine("Description: " + evianXBoolean.Description);
+            Console.WriteLine("Price: " + evianXBoolean.GetPrice() + "$");
+            Console.WriteLine("Price whit IVA: " + evianXBoolean.PriceWhitIva() + "$");
+            Console.WriteLine("Extended name: " + evianXBoolean.ExtendedName());
         }
     }
 }
